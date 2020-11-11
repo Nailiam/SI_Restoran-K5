fcb5c0af866024e57b44597bd97f601d85d5a5aa
Nadia1944 <63362943+Nadia1944@users.noreply.github.com>
2020-11-12 03:00:05 +0700

    benerin form pesan

--- WindowsApplication1/Pesan.Designer.vb
+++ WindowsApplication1/Pesan.Designer.vb
@@ -32,13 +32,9 @@ Partial Class Pesan
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
-        Me.harga_minuman = New System.Windows.Forms.TextBox()
-        Me.harga_makanan = New System.Windows.Forms.TextBox()
         Me.jumlah_pemesanan = New System.Windows.Forms.TextBox()
-        Me.nama_pemesan = New System.Windows.Forms.TextBox()
+        Me.ID_Pelanggan = New System.Windows.Forms.TextBox()
         Me.kode_pemesanan = New System.Windows.Forms.TextBox()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
@@ -51,7 +47,7 @@ Partial Class Pesan
         '
         'Button6
         '
-        Me.Button6.Location = New System.Drawing.Point(220, 325)
+        Me.Button6.Location = New System.Drawing.Point(220, 280)
         Me.Button6.Name = "Button6"
         Me.Button6.Size = New System.Drawing.Size(57, 28)
         Me.Button6.TabIndex = 15
@@ -60,7 +56,7 @@ Partial Class Pesan
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(136, 325)
+        Me.Button5.Location = New System.Drawing.Point(136, 280)
         Me.Button5.Name = "Button5"
         Me.Button5.Size = New System.Drawing.Size(57, 28)
         Me.Button5.TabIndex = 14
@@ -69,7 +65,7 @@ Partial Class Pesan
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(51, 325)
+        Me.Button4.Location = New System.Drawing.Point(51, 280)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(57, 28)
         Me.Button4.TabIndex = 13
@@ -78,7 +74,7 @@ Partial Class Pesan
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(220, 280)
+        Me.Button3.Location = New System.Drawing.Point(220, 230)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(57, 28)
         Me.Button3.TabIndex = 12
@@ -87,7 +83,7 @@ Partial Class Pesan
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(136, 280)
+        Me.Button2.Location = New System.Drawing.Point(136, 230)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(57, 28)
         Me.Button2.TabIndex = 11
@@ -96,7 +92,7 @@ Partial Class Pesan
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(51, 280)
+        Me.Button1.Location = New System.Drawing.Point(51, 230)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(57, 28)
         Me.Button1.TabIndex = 10
@@ -111,7 +107,7 @@ Partial Class Pesan
         Me.GroupBox2.Size = New System.Drawing.Size(415, 323)
         Me.GroupBox2.TabIndex = 9
         Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "Detail Pemesanan"
+        Me.GroupBox2.Text = "Detail Pesanan"
         '
         'DataGridView1
         '
@@ -124,23 +120,19 @@ Partial Class Pesan
         'GroupBox1
         '
         Me.GroupBox1.Controls.Add(Me.DateTimePicker1)
-        Me.GroupBox1.Controls.Add(Me.harga_minuman)
-        Me.GroupBox1.Controls.Add(Me.harga_makanan)
         Me.GroupBox1.Controls.Add(Me.jumlah_pemesanan)
-        Me.GroupBox1.Controls.Add(Me.nama_pemesan)
+        Me.GroupBox1.Controls.Add(Me.ID_Pelanggan)
         Me.GroupBox1.Controls.Add(Me.kode_pemesanan)
-        Me.GroupBox1.Controls.Add(Me.Label6)
-        Me.GroupBox1.Controls.Add(Me.Label5)
         Me.GroupBox1.Controls.Add(Me.Label4)
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Location = New System.Drawing.Point(25, 30)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(311, 235)
+        Me.GroupBox1.Size = New System.Drawing.Size(311, 185)
         Me.GroupBox1.TabIndex = 8
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Input Data Pemesanan"
+        Me.GroupBox1.Text = "Input Data Pesanan"
         '
         'DateTimePicker1
         '
@@ -149,20 +141,6 @@ Partial Class Pesan
         Me.DateTimePicker1.Size = New System.Drawing.Size(117, 20)
         Me.DateTimePicker1.TabIndex = 11
         '
-        'harga_minuman
-        '
-        Me.harga_minuman.Location = New System.Drawing.Point(151, 185)
-        Me.harga_minuman.Name = "harga_minuman"
-        Me.harga_minuman.Size = New System.Drawing.Size(117, 20)
-        Me.harga_minuman.TabIndex = 10
-        '
-        'harga_makanan
-        '
-        Me.harga_makanan.Location = New System.Drawing.Point(151, 150)
-        Me.harga_makanan.Name = "harga_makanan"
-        Me.harga_makanan.Size = New System.Drawing.Size(117, 20)
-        Me.harga_makanan.TabIndex = 9
-        '
         'jumlah_pemesanan
         '
         Me.jumlah_pemesanan.Location = New System.Drawing.Point(151, 117)
@@ -170,12 +148,12 @@ Partial Class Pesan
         Me.jumlah_pemesanan.Size = New System.Drawing.Size(117, 20)
         Me.jumlah_pemesanan.TabIndex = 8
         '
-        'nama_pemesan
+        'ID_Pelanggan
         '
-        Me.nama_pemesan.Location = New System.Drawing.Point(151, 56)
-        Me.nama_pemesan.Name = "nama_pemesan"
-        Me.nama_pemesan.Size = New System.Drawing.Size(117, 20)
-        Me.nama_pemesan.TabIndex = 7
+        Me.ID_Pelanggan.Location = New System.Drawing.Point(151, 56)
+        Me.ID_Pelanggan.Name = "ID_Pelanggan"
+        Me.ID_Pelanggan.Size = New System.Drawing.Size(117, 20)
+        Me.ID_Pelanggan.TabIndex = 7
         '
         'kode_pemesanan
         '
@@ -184,63 +162,45 @@ Partial Class Pesan
         Me.kode_pemesanan.Size = New System.Drawing.Size(117, 20)
         Me.kode_pemesanan.TabIndex = 6
         '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(23, 192)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(81, 13)
-        Me.Label6.TabIndex = 5
-        Me.Label6.Text = "Harga minuman"
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(23, 157)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(84, 13)
-        Me.Label5.TabIndex = 4
-        Me.Label5.Text = "Harga Makanan"
-        '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Location = New System.Drawing.Point(23, 124)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(99, 13)
+        Me.Label4.Size = New System.Drawing.Size(85, 13)
         Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Jumlah Pemesanan"
+        Me.Label4.Text = "Jumlah Pesanan"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Location = New System.Drawing.Point(23, 93)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(105, 13)
+        Me.Label3.Size = New System.Drawing.Size(91, 13)
         Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Tanggal Pemesanan"
+        Me.Label3.Text = "Tanggal Pesanan"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Location = New System.Drawing.Point(23, 63)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(82, 13)
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
         Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Nama Pemesan"
+        Me.Label2.Text = "ID Pelanggan"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(23, 30)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(91, 13)
+        Me.Label1.Size = New System.Drawing.Size(77, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Kode Pemesanan"
+        Me.Label1.Text = "Kode Pesanan"
         '
         'Button7
         '
-        Me.Button7.Location = New System.Drawing.Point(674, 375)
+        Me.Button7.Location = New System.Drawing.Point(674, 366)
         Me.Button7.Name = "Button7"
         Me.Button7.Size = New System.Drawing.Size(100, 28)
         Me.Button7.TabIndex = 16
@@ -251,7 +211,7 @@ Partial Class Pesan
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 426)
+        Me.ClientSize = New System.Drawing.Size(800, 406)
         Me.Controls.Add(Me.Button7)
         Me.Controls.Add(Me.Button6)
         Me.Controls.Add(Me.Button5)
@@ -281,13 +241,9 @@ Partial Class Pesan
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents DateTimePicker1 As DateTimePicker
-    Friend WithEvents harga_minuman As TextBox
-    Friend WithEvents harga_makanan As TextBox
     Friend WithEvents jumlah_pemesanan As TextBox
-    Friend WithEvents nama_pemesan As TextBox
+    Friend WithEvents ID_Pelanggan As TextBox
     Friend WithEvents kode_pemesanan As TextBox
-    Friend WithEvents Label6 As Label
-    Friend WithEvents Label5 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label2 As Label
--- WindowsApplication1/Pesan.vb
+++ WindowsApplication1/Pesan.vb
@@ -5,24 +5,18 @@
     End Sub
     Sub KosongkanForm()
         kode_pemesanan.Text = ""
-        nama_pemesan.Text = ""
+        ID_Pelanggan.Text = ""
         jumlah_pemesanan.Text = ""
-        harga_makanan.Text = ""
-        harga_minuman.Text = ""
     End Sub
     Sub MatikanForm()
         kode_pemesanan.Enabled = False
-        nama_pemesan.Enabled = False
+        ID_Pelanggan.Enabled = False
         jumlah_pemesanan.Enabled = False
-        harga_makanan.Enabled = False
-        harga_minuman.Enabled = False
     End Sub
     Sub HidupkanForm()
         kode_pemesanan.Enabled = True
-        nama_pemesan.Enabled = True
+        ID_Pelanggan.Enabled = True
         jumlah_pemesanan.Enabled = True
-        harga_makanan.Enabled = True
-        harga_minuman.Enabled = True
     End Sub
     Sub TampilkanData()
         Call koneksiDB()
@@ -48,19 +42,19 @@
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        If kode_pemesanan.Text = "" Or nama_pemesan.Text = "" Or jumlah_pemesanan.Text = "" Or harga_makanan.Text = "" Or harga_minuman.Text = "" Then
-            MsgBox("Data Pemesanan Belum Lengkap")
+        If kode_pemesanan.Text = "" Or ID_Pelanggan.Text = "" Or jumlah_pemesanan.Text = "" Then
+            MsgBox("Data Pesanan Belum Lengkap")
             Exit Sub
         Else
             Call koneksiDB()
-            CMD = New OleDb.OleDbCommand(" select * from Pesan where kode_pemesanan ='" & kode_pemesanan.Text & "'", Conn)
+            CMD = New OleDb.OleDbCommand(" select * from Pesan where kode_pesanan ='" & kode_pemesanan.Text & "'", Conn)
             DM = CMD.ExecuteReader
             DM.Read()
             If Not DM.HasRows Then
                 Call koneksiDB()
                 Dim simpan As String
                 simpan = "insert into Pesan values ('" & kode_pemesanan.Text &
-               "', '" & nama_pemesan.Text & "', '" & jumlah_pemesanan.Text & "', '" & harga_makanan.Text & "','" & harga_minuman.Text & "')"
+               "', '" & ID_Pelanggan.Text & "', '" & jumlah_pemesanan.Text & "')"
                 CMD = New OleDb.OleDbCommand(simpan, Conn)
                 CMD.ExecuteNonQuery()
                 MsgBox("Input Data Sukses")
@@ -74,13 +68,13 @@
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        If kode_pemesanan.Text = "" Or nama_pemesan.Text = "" Or jumlah_pemesanan.Text = "" Or harga_makanan.Text = "" Or harga_minuman.Text = "" Then
-            MsgBox("Data Pemesanan Belum Lengkap")
+        If kode_pemesanan.Text = "" Or ID_Pelanggan.Text = "" Or jumlah_pemesanan.Text = "" Then
+            MsgBox("Data Pesanan Belum Lengkap")
             Exit Sub
         Else
             Call koneksiDB()
-            CMD = New OleDb.OleDbCommand("update pesan set nama_pemesan = '" &
-           nama_pemesan.Text & "', jumlah_pemesanan = '" & jumlah_pemesanan.Text & "', harga_makanan = '" & harga_makanan.Text & "', harga_minuman = '" & harga_minuman.Text & "'", Conn)
+            CMD = New OleDb.OleDbCommand("update Pesan set ID_Pelanggan = '" &
+           ID_Pelanggan.Text & "', jumlah_pesanan = '" & jumlah_pemesanan.Text & "' where kode_pesanan ='" & kode_pemesanan.Text & "'", Conn)
             DM = CMD.ExecuteReader
             MsgBox("Update Data Berhasil")
         End If
@@ -96,7 +90,7 @@
         Else
             If MessageBox.Show(" Are you sure to delete this data?", "Konfirmasi", MessageBoxButtons.YesNoCancel) Then
                 Call koneksiDB()
-                CMD = New OleDb.OleDbCommand(" delete from Pesan where kode_pemesanan = '" & kode_pemesanan.Text & "'", Conn)
+                CMD = New OleDb.OleDbCommand(" delete from Pesan where kode_pesanan = '" & kode_pemesanan.Text & "'", Conn)
                 DM = CMD.ExecuteReader
                 MsgBox("Hapus Data Berhasil")
                 Call MatikanForm()
@@ -112,18 +106,17 @@
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
         On Error Resume Next
         kode_pemesanan.Text = DataGridView1.Rows(e.RowIndex).Cells(0).Value
-        nama_pemesan.Text = DataGridView1.Rows(e.RowIndex).Cells(1).Value
+        ID_Pelanggan.Text = DataGridView1.Rows(e.RowIndex).Cells(1).Value
         DateTimePicker1.Format = DateTimePickerFormat.Custom
         DateTimePicker1.CustomFormat = "dddd, dd/MM/yyyy"
         DateTimePicker1.Value = Format(DataGridView1.Rows(e.RowIndex).Cells(2).Value)
         jumlah_pemesanan.Text = DataGridView1.Rows(e.RowIndex).Cells(3).Value
-        harga_makanan.Text = DataGridView1.Rows(e.RowIndex).Cells(4).Value
-        harga_minuman.Text = DataGridView1.Rows(e.RowIndex).Cells(5).Value
+
         Call HidupkanForm()
         kode_pemesanan.Enabled = False
     End Sub
 
     Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
-        Bayar.Show()
+        Kasir.Show()
     End Sub
 End Class
